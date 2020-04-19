cf073597e1f9a31a339bed6a9b1f64ac8abfca31
Dickinson Arismendy <darismendy@apricoot.com>
2020-04-19 11:53:52 -0500

    fix: se agrega la tarea default para gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
+gulp.task ("default", function() {});
+
 gulp.task("licenses", async function() {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
