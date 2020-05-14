b5a4e3f704afea4aa9263d101df7301c2f49a912
Laura Hurtado <shurtaddo@gmail.com>
2020-05-14 08:47:04 -0500

    Prefunctional

--- src/views/Mascotas/Mascotas.js
+++ src/views/Mascotas/Mascotas.js
@@ -1,61 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { makeStyles } from "@material-ui/core/styles";
-import '../styles/Badges.css';
-// import confLogo from '../images/badge-header.svg';
-import ListarMascotas from '../../components/ListarMascotas';
-//import { Grid } from '@material-ui/core';
-import Card from "components/Card/Card.js";
-import CardHeader from "components/Card/Card.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import MyPetAPI from 'MyPetAPI';
-
-import styles from "assets/jss/material-dashboard-react/views/iconsStyle.js";
-import Primary from 'components/Typography/Primary';
-import buttonStyle from 'assets/jss/material-dashboard-react/components/buttonStyle';
 import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+import PropTypes from 'prop-types';
+import { default as Card, default as CardHeader } from "components/Card/Card.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Primary from 'components/Typography/Primary';
+import MyPetAPI from 'MyPetAPI';
+import React from 'react';
+import ListarMascotas from '../../components/ListarMascotas';
+import '../styles/Badges.css';
 
-const useStyles = makeStyles(styles);
 
 
-// const styles = {
-//   cardCategoryWhite: {
-//     color: "rgba(255,255,255,.62)",
-//     margin: "0",
-//     fontSize: "14px",
-//     marginTop: "0",
-//     marginBottom: "0"
-//   },
-//   cardTitleWhite: {
-//     color: "#FFFFFF",
-//     marginTop: "0px",
-//     minHeight: "auto",
-//     fontWeight: "300",
-//     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-//     marginBottom: "3px",
-//     textDecoration: "none"
-//   }
-// };
-
-// const styles = makeStyles(styles);
-
-// const classes = useStyles();
+const styles = theme => ({
+});
 
 class Mascotas extends React.Component {
-  
+
   constructor(props) {
     super(props);
     console.log('1. constructor()');
-
     this.state = {
       data: [],
     };
   }
 
   cargar = async () => {
-    let data = await MyPetAPI.mascotas.porPersona(1);    
-    
-    this.setState({data: data})
+    let data = await MyPetAPI.mascotas.porPersona(1);
+    this.setState({ data: data })
   }
 
   componentDidMount() {
@@ -69,7 +40,6 @@ class Mascotas extends React.Component {
       prevProps: prevProps,
       prevState: prevState,
     });
-
     console.log({
       props: this.props,
       state: this.state,
@@ -81,34 +51,32 @@ class Mascotas extends React.Component {
     clearTimeout(this.timeoutId);
   }
 
-  
-
   render() {
-    // const classes = useStyles();
     return (
-
       <React.Fragment>
         <GridContainer>
-        <Card color = {Primary}>
-        <CardHeader color = "primary">
-              
-
-        <h2>Mis Mascotas</h2>
-              
-
+          <Card color={Primary}>
+            <CardHeader color="primary">
+              <h2>Mis Mascotas</h2>
             </CardHeader>
-          <div className = "Badges_buttons">
-            <Button  color = "primary" >
-              Nueva Mascota
-            </ Button> 
-          </div>
-          <ListarMascotas mascotas={this.state.data} />
-         </Card>
-         
+            <div className="Badges_buttons">
+              <Button color="primary"
+                onClick={() => window.location.href = '/admin/RegistroMascotas'}
+              >
+                Nueva Mascota
+            </ Button>
+            </div>
+            <ListarMascotas mascotas={this.state.data} />
+          </Card>
+
         </GridContainer>
       </React.Fragment>
     );
   }
 }
 
-export default Mascotas;
+Mascotas.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(Mascotas);
